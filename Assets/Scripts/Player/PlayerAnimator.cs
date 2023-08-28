afb934283de571e465c6326972259cd088939cb2
misswarrior <maripsan04@gmail.com>
2023-08-27 21:35:09 -0300

    Add Idle And Walk Animation And Step Particle

--- Assets/Scripts/Player/PlayerAnimator.cs
+++ Assets/Scripts/Player/PlayerAnimator.cs
@@ -4,7 +4,16 @@ using UnityEngine;
 
 public class PlayerAnimator : MonoBehaviour
 {
+    private Rigidbody _rb;
+
     [SerializeField] private GameObject body;
+    private Animator _bodyAnimator;
+
+    private void Start()
+    {
+        _rb = GetComponentInParent<Rigidbody>();
+        _bodyAnimator = body.GetComponent<Animator>();
+    }
 
     public void SetBody(GameObject skin)
     {
@@ -14,5 +23,15 @@ public class PlayerAnimator : MonoBehaviour
         }
         GameObject bodyNewChild = Instantiate(skin, body.transform.position, body.transform.rotation);
         bodyNewChild.transform.SetParent(body.transform);
+    }
+
+    private void Update()
+    {
+        SetAnimatorVariable();
+    }
+
+    private void SetAnimatorVariable()
+    {
+        _bodyAnimator.SetBool("isMoving", _rb.velocity.magnitude > 0.5);
     }
 }
